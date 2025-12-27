cf2ce688cac8adca6800502b10c6968c83aa7c74
bun349 <bunga05adlyna@gmail.com>
2025-12-27 15:04:49 +0700

    voice lebih indo

--- constant.js
+++ constant.js
@@ -5,4 +5,4 @@ dotenv.config();
 
 export const geminiApiKey = process.env.GEMINI_API_KEY;
 export const elevenLabsApiKey = process.env.ELEVEN_LABS_API_KEY;
-export const voiceID = "21m00Tcm4TlvDq8ikWAM";
+export const voiceID = "fUesUKVrbYRcEnWoLXet";
