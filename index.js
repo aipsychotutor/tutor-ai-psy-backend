b1e5236051ac07a03fe85cdd279c007274839afb
OdeChaerani <zachra1717@gmail.com>
2025-12-27 11:01:04 +0700

    .

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ const port = 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+    "http://localhost:5173", 
+    "http://localhost:5174" 
+    ],
     credentials: true,
   })
 );
